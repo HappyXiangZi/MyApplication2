242efbcac5adea891be8ca3c7fea2250d8b61772
zengyixiang <1305757203@qq.com>
2019-03-19 11:32:42 +0800

    add mainfest

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public final static String TAG = "AndroidLiveCycle";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constrainlayout);
+        setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate");
+
+        Button linear = findViewById(R.id.Linear);
+        linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,LinearActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button  table = findViewById(R.id.table);
+        table.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button constrain = findViewById(R.id.constrain);
+        constrain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,Constrain.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -48,4 +83,5 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d(TAG,"onDestroy");
     }
+
 }
